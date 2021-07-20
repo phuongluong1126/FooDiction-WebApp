713391aa24956900d52a7b96688bc28129766fc9
phuong luong <email@example.com>
2021-07-21 02:36:06 +0700

    update video

--- src/components/model/HeroSection.js
+++ src/components/model/HeroSection.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button } from '../model/Button';
 import './HeroSection.css';
 import { Link } from 'react-router-dom';
+import intro from "../../assets/video/intro.mp4";
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src= {intro} autoPlay loop muted />
       <h1>FOOD DICTIONARY</h1>
       {/* <p>What are you waiting for?</p> */}
       <Link to='/explore' className='hero-btns'>
